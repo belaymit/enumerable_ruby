6d5b15d9f59283ebbca54cdcf6b77fec16bee8f1
Jules Edozie <edoziechukwuma@gmail.com>
2023-03-03 00:13:06 +0100

    Added any? and filter methods

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -5,15 +5,15 @@ module MyEnumerable
     p list_of_nums.length == list.length
   end
 
-  # def any?
-  #   list_of_nums = []
-  #   list.each { |n| list_of_nums.push(n) if yield n }
-  #   p !list_of_nums.empty?
-  # end
+  def any?
+    list_of_nums = []
+    list.each { |n| list_of_nums.push(n) if yield n }
+    p !list_of_nums.empty?
+  end
 
-  # def filter
-  #   list_of_nums = []
-  #   list.each { |n| list_of_nums.push(n) if yield n }
-  #   p list_of_nums
-  # end
+  def filter
+    list_of_nums = []
+    list.each { |n| list_of_nums.push(n) if yield n }
+    p list_of_nums
+  end
 end
