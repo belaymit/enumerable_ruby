4e532ee316f2459c3c5c96c63a72db7dc5736a62
belaymit <2belamit@gmail.com>
2023-03-03 08:45:17 +0300

    changed list name to MyList as required

--- list.rb
+++ list.rb
@@ -1,28 +1,28 @@
 require_relative './my_enumerable'
-class Lsit
+class MyLsit
   include MyEnumerable
   def initialize(*list)
     @list = list
   end
 
-  attr_accessor :list
+  attr_reader :list
 
   def each
     yield @list
   end
 end
 
-list_of_nums = Lsit.new(1, 2, 3, 4, 5, 6, 7)
+list_of_nums = MyLsit.new(1, 2, 3, 4)
 
 puts list_of_nums.list
 
 # Test all
-list_of_nums.all? { |e| e < 8 } # true
-list_of_nums.all? { |e| e > 8 } # false
+list_of_nums.all? { |e| e < 5 } # true
+list_of_nums.all? { |e| e > 5 } # false
 
 # Test any?
 list_of_nums.any? { |e| e == 2 } # true
-list_of_nums.any? { |e| e == 8 } # false
+list_of_nums.any? { |e| e == 5 } # false
 
 # Test filter
-list_of_nums.filter(&:even?) # [2, 4, 6]
+list_of_nums.filter(&:even?) # [2, 4]
